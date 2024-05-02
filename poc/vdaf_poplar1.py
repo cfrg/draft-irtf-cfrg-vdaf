bd2db33cbf72a6febeffb8951f1d8e5fad509ee5
Phillipp Schoppmann <schoppmp@users.noreply.github.com>
2024-05-02 17:04:07 -0400

    Update poc/vdaf_poplar1.py
    Co-authored-by: David Cook <dcook@divviup.org>

--- poc/vdaf_poplar1.py
+++ poc/vdaf_poplar1.py
@@ -170,8 +170,7 @@ class Poplar1(Vdaf):
 
         (level, prefixes) = agg_param
         (last_level, last_prefixes) = previous_agg_params[-1]
-        # The empty prefix 0 is always there.
-        last_prefixes_set = set(list(last_prefixes) + [0])
+        last_prefixes_set = set(last_prefixes)
 
         # Check that level increased.
         if level <= last_level:
