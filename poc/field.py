7a1bf5dbbbec61d7ade2461648aaa63efa405e79
David Cook <dcook@divviup.org>
2024-06-20 09:45:25 -0500

    Revamp type hints in field.py, fix shadowing

--- poc/field.py
+++ poc/field.py
@@ -1,8 +1,9 @@
 """Definitions of finite fields used in this spec."""
 
-from __future__ import annotations
+from typing import Self, TypeVar
 
-from sage.all import GF, PolynomialRing  # type: ignore
+from sage.all import GF, PolynomialRing
+from sage.rings.finite_rings.finite_field_constructor import FiniteFieldFactory
 
 from common import from_le_bytes, to_le_bytes
 
@@ -16,17 +17,19 @@ class Field:
     # Number of bytes used to encode each field element.
     ENCODED_SIZE: int
 
+    gf: FiniteFieldFactory
+
     def __init__(self, val):
         assert int(val) < self.MODULUS
         self.val = self.gf(val)
 
     @classmethod
-    def zeros(cls, length):
+    def zeros(cls, length: int) -> list[Self]:
         vec = [cls(cls.gf.zero()) for _ in range(length)]
         return vec
 
     @classmethod
-    def rand_vec(cls, length):
+    def rand_vec(cls, length: int) -> list[Self]:
         """
         Return a random vector of field elements of length `length`.
         """
@@ -34,21 +37,21 @@ class Field:
         return vec
 
     @classmethod
-    def encode_vec(Field, vec: list[Field]) -> bytes:
+    def encode_vec(cls, vec: list[Self]) -> bytes:
         """
         Encode a vector of field elements `vec` as a byte string.
         """
         encoded = bytes()
         for x in vec:
-            encoded += to_le_bytes(x.as_unsigned(), Field.ENCODED_SIZE)
+            encoded += to_le_bytes(x.as_unsigned(), cls.ENCODED_SIZE)
         return encoded
 
     @classmethod
-    def decode_vec(Field, encoded: bytes) -> list[Field]:
+    def decode_vec(cls, encoded: bytes) -> list[Self]:
         """
         Parse a vector of field elements from `encoded`.
         """
-        L = Field.ENCODED_SIZE
+        L = cls.ENCODED_SIZE
         if len(encoded) % L != 0:
             raise ValueError(
                 'input length must be a multiple of the size of an '
@@ -58,15 +61,16 @@ class Field:
         for i in range(0, len(encoded), L):
             encoded_x = encoded[i:i+L]
             x = from_le_bytes(encoded_x)
-            if x >= Field.MODULUS:
+            if x >= cls.MODULUS:
                 raise ValueError('modulus overflow')
-            vec.append(Field(x))
+            vec.append(cls(x))
         return vec
 
     @classmethod
-    def encode_into_bit_vector(Field,
-                               val: int,
-                               bits: int) -> list[Field]:
+    def encode_into_bit_vector(
+            cls,
+            val: int,
+            bits: int) -> list[Self]:
         """
         Encode the bit representation of `val` with at most `bits` number
         of bits, as a vector of field elements.
@@ -83,55 +87,55 @@ class Field:
                              "the input integer.")
         encoded = []
         for l in range(bits):
-            encoded.append(Field((val >> l) & 1))
+            encoded.append(cls((val >> l) & 1))
         return encoded
 
     @classmethod
-    def decode_from_bit_vector(Field, vec: list[Field]) -> Field:
+    def decode_from_bit_vector(cls, vec: list[Self]) -> Self:
         """
         Decode the field element from the bit representation, expressed
         as a vector of field elements `vec`.
         """
         bits = len(vec)
-        if Field.MODULUS >> bits == 0:
+        if cls.MODULUS >> bits == 0:
             raise ValueError("Number of bits is too large to be "
                              "represented by field modulus.")
-        decoded = Field(0)
+        decoded = cls(0)
         for (l, bit) in enumerate(vec):
-            decoded += Field(1 << l) * bit
+            decoded += cls(1 << l) * bit
         return decoded
 
-    def __add__(self, other: Field) -> Field:
+    def __add__(self, other: Self) -> Self:
         return self.__class__(self.val + other.val)
 
-    def __neg__(self) -> Field:
+    def __neg__(self) -> Self:
         return self.__class__(-self.val)
 
-    def __mul__(self, other: Field) -> Field:
+    def __mul__(self, other: Self) -> Self:
         return self.__class__(self.val * other.val)
 
-    def inv(self):
+    def inv(self) -> Self:
         return self.__class__(self.val**-1)
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.val == other.val
 
-    def __sub__(self, other):
+    def __sub__(self, other: Self) -> Self:
         return self + (-other)
 
-    def __div__(self, other):
+    def __div__(self, other: Self) -> Self:
         return self * other.inv()
 
-    def __pow__(self, n):
+    def __pow__(self, n: int) -> Self:
         return self.__class__(self.val ** n)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.val)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return str(self.val)
 
-    def as_unsigned(self):
+    def as_unsigned(self) -> int:
         return int(self.gf(self.val))
 
 
@@ -140,7 +144,7 @@ class FftField(Field):
     GEN_ORDER: int
 
     @classmethod
-    def gen(cls) -> Field:
+    def gen(cls) -> Self:
         raise NotImplementedError()
 
 
@@ -182,7 +186,7 @@ class Field64(FftField):
     gf = GF(MODULUS)
 
     @classmethod
-    def gen(cls):
+    def gen(cls) -> Self:
         return cls(7)**4294967295
 
 
@@ -197,7 +201,7 @@ class Field96(FftField):
     gf = GF(MODULUS)
 
     @classmethod
-    def gen(cls):
+    def gen(cls) -> Self:
         return cls(3)**4294966555
 
 
@@ -212,7 +216,7 @@ class Field128(FftField):
     gf = GF(MODULUS)
 
     @classmethod
-    def gen(cls):
+    def gen(cls) -> Self:
         return cls(7)**4611686018427387897
 
 
@@ -230,30 +234,32 @@ class Field255(Field):
 # POLYNOMIAL ARITHMETIC
 #
 
+F = TypeVar("F", bound=Field)
 
-def poly_strip(Field, p):
+
+def poly_strip(field: type[F], p: list[F]) -> list[F]:
     """Remove leading zeros from the input polynomial."""
     for i in reversed(range(len(p))):
-        if p[i] != Field(0):
+        if p[i] != field(0):
             return p[:i+1]
     return []
 
 
-def poly_mul(Field, p, q):
+def poly_mul(field: type[F], p: list[F], q: list[F]) -> list[F]:
     """Multiply two polynomials."""
-    r = [Field(0) for _ in range(len(p) + len(q))]
+    r = [field(0) for _ in range(len(p) + len(q))]
     for i in range(len(p)):
         for j in range(len(q)):
             r[i + j] += p[i] * q[j]
-    return poly_strip(Field, r)
+    return poly_strip(field, r)
 
 
-def poly_eval(Field, p, eval_at):
+def poly_eval(field: type[F], p: list[F], eval_at: F) -> F:
     """Evaluate a polynomial at a point."""
     if len(p) == 0:
-        return Field(0)
+        return field(0)
 
-    p = poly_strip(Field, p)
+    p = poly_strip(field, p)
     result = p[-1]
     for c in reversed(p[:-1]):
         result *= eval_at
@@ -262,8 +268,8 @@ def poly_eval(Field, p, eval_at):
     return result
 
 
-def poly_interp(Field, xs, ys):
+def poly_interp(field: type[F], xs: list[F], ys: list[F]) -> list[F]:
     """Compute the Lagrange interpolation polynomial for the given points."""
-    R = PolynomialRing(Field.gf, 'x')
+    R = PolynomialRing(field.gf, 'x')
     p = R.lagrange_polynomial([(x.val, y.val) for (x, y) in zip(xs, ys)])
-    return poly_strip(Field, list(map(lambda x: Field(x), p.coefficients())))
+    return poly_strip(field, list(map(lambda x: field(x), p.coefficients())))
